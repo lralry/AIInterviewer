8bd95a5654fc6e6bf2a2c620d589b19c1d8e7a78
lralry <1531801102@qq.com>
2024-04-20 01:47:12 +0800

    更新

--- AIInterviewer/main.py
+++ AIInterviewer/main.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from flask import Flask,request,render_template,flash
 from model.config import flask_default
-from flask import render_template
 from view.momgodb_do import mongo_view
 
 # 实例化一个Flask类的对象
 app = Flask(__name__)
-
-
 # 设置路由和函数对应关系
 @app.route('/')
 def web1():
@@ -15,6 +12,8 @@ def web1():
 @app.route('/b')
 def web2():
     return render_template("echarts6.html")
+
+
 
 #run
 if __name__ == '__main__':
