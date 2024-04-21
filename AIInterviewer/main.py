e0fff43550cb5e13131bcbbefdc295d8ea35424f
lralry <1531801102@qq.com>
2024-04-21 14:06:03 +0800

    测试

--- AIInterviewer/main.py
+++ AIInterviewer/main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for,flask
 #from view.momgodb_do import mongo_view
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")  # 地址与接口
+client = MongoClient('mongodb://localhost:27017')  # 地址与接口
 database = client.get_database('test')  # 数据库名
 collection = database.get_collection('test_q')  # 表名
 
@@ -16,23 +16,25 @@ app = Flask(__name__)
 def web1():
     return render_template("web.html")
 
-@app.route('/b')
+@app.route('/result',methods = ['POST', 'GET'])
 def web2():
     return render_template("echarts6.html")
 
 
-@app.route('/c')
-def forensic():
-    print('this is forensic ,waiting a moment')
-    # 数据库信息
-    listx = []
-    # 查询数据库
-    for x in mycol.find({}, {"_id": 0}):
-        print(x)
-        listx.append(x)
-    print(listx)
-    # 将数据库信息转换为Json格式传给前端
-    return jsonify(listx)
+@app.route('/inser_test')
+def inser_data():
+    user = {'name': 'John Doe', 'age': 25, 'city': 'New York'}
+    collection.insert_one(user)
+    return 'Data inserted successfully!'
+
+@app.route('/query')
+def query_data():
+    users = collection.find()
+    result = ''
+    for user in users:
+        result += f"Name: {user['name']}, Age: {user['age']}, City: {user['city']}<br>"
+    return result
+
 
 
 #run
